95b92f59536dbd896b3788695f6a0c62f6028bed
frederickschulze <frederick9991@gmail.com>
2026-05-21 13:24:15 -0500

    Lunch 5-21-2026

--- Chessboard.py
+++ Chessboard.py
@@ -41,7 +41,7 @@ class Chessboard:
 
     def print_board(self):
         i = 0
-        for char in self.squares:
+        for char in reversed(self.squares):
             print(char, end="")
             i += 1
             if i >= 8:
@@ -49,11 +49,40 @@ class Chessboard:
                 print()
 
 
+    def set_squares(self, value: int, start_coords: str, end_coords = None):
+        start_index = self.get_square_index(start_coords)
+        if end_coords is not None:
+            end_index = self.get_square_index(end_coords)
+            if start_index > end_index:
+                start_index, end_index = end_index, start_index
+            self.squares[start_index:end_index+1] = [value]*(end_index-start_index+1)
+        else:
+            self.squares[start_index] = value
+
     @staticmethod
     def get_square_name(i: int):
         rank = i // 8 # Floor division returns row
         row = i % 8 # Remainder returns column
         return(rank, row)
+
+    @staticmethod
+    def get_square_index(coords: str) -> int:
+        fileNames = "abcdefgh"
+        rankNames = "12345678"
+        if (not isinstance(coords, str)
+            or len(coords) != 2
+            or coords[0] not in fileNames
+            or coords[1] not in rankNames):
+            raise ValueError("The input coords are not in the correct format")
+        
+        fileNum = fileNames.index(coords[0])
+        rankNum = int(coords[1])
+
+        overall_index = 8*(rankNum-1)+fileNum
+
+        return overall_index
+    
+        
 
     def set_board_state(self, board: str):
         self.squares = self.squares
@@ -66,5 +95,9 @@ print(board.get_square_name(7))
 print(board.get_square_name(8))
 print(board.get_square_name(63))
 print(board.get_square_name(64))
+
+board.print_board()
+
+board.set_squares(1, "f4")
 
 board.print_board()
