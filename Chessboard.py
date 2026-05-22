8220d9aa4bb32d3739a2956d78b891cf505d4df7
frederickschulze <frederick9991@gmail.com>
2026-05-22 15:08:25 -0500

    Coffee break 5-22-2026

--- Chessboard.py
+++ Chessboard.py
@@ -1,5 +1,8 @@
 # This file describes the chessboard class
 import numpy as np
+from stockfish import Stockfish
+
+stockfish = Stockfish(path="C:\Users\frede\Documents\Code Projects\ChessAI\engines\stockfish-5.2.0")
 
 WHITE = 0
 BLACK = 1
@@ -20,10 +23,11 @@ class Chessboard:
     # Rook: 4
     # Queen: 5
     # King: 6
+    # WHITE: Positive, BLACK: Negative
 
     def __init__(self, layout: list[int]):
         if len(layout) != 64:
-            raise ValueError("ValueError: The requested layout does not have 64 squares.")
+            raise ValueError("The requested layout does not have 64 squares.")
         else:
             self.squares = layout.copy()
     
@@ -37,22 +41,37 @@ class Chessboard:
         squares = [0]*64
         squares[0:8] = [4, 2, 3, 5, 6, 3, 2, 4]
         squares[8:16] = [1, 1, 1, 1, 1, 1,1, 1]
+        squares[Chessboard.coords_to_index("a7"):Chessboard.coords_to_index("h7")+1] = [1]*8
+        squares[Chessboard.coords_to_index("a8"):Chessboard.coords_to_index("h8")+1] = [4, 2, 3, 5, 6, 3, 2, 4]
         return cls(squares)
 
-    def print_board(self):
+    def print_board(self, flipped: bool = False):
         i = 0
-        for char in reversed(self.squares):
+        local_flipped_squares = self.squares
+        if flipped:
+            local_flipped_squares = self.flip_vertically(local_flipped_squares)
+        for char in local_flipped_squares:
             print(char, end="")
             i += 1
             if i >= 8:
                 i = 0
                 print()
+    
+    @staticmethod
+    def flip_vertically(squares: list[int]) -> list[int]:
+        if len(squares) != 64:
+            raise ValueError("The intput layout does not have 64 squares.")
+        
+        flippedSquares = [0]*64
+        for rank in range(0,8):
+            flippedSquares[8*rank:(8*(rank+1))] = squares[(8*(7-rank)):(8*(8-rank))]
+        return flippedSquares
+        
 
-
-    def set_squares(self, value: int, start_coords: str, end_coords = None):
-        start_index = self.get_square_index(start_coords)
+    def set_square(self, value: int, start_coords: str, end_coords = None):
+        start_index = self.coords_to_index(start_coords)
         if end_coords is not None:
-            end_index = self.get_square_index(end_coords)
+            end_index = self.coords_to_index(end_coords)
             if start_index > end_index:
                 start_index, end_index = end_index, start_index
             self.squares[start_index:end_index+1] = [value]*(end_index-start_index+1)
@@ -66,7 +85,7 @@ class Chessboard:
         return(rank, row)
 
     @staticmethod
-    def get_square_index(coords: str) -> int:
+    def coords_to_index(coords: str) -> int:
         fileNames = "abcdefgh"
         rankNames = "12345678"
         if (not isinstance(coords, str)
@@ -82,7 +101,7 @@ class Chessboard:
 
         return overall_index
     
-        
+   
 
     def set_board_state(self, board: str):
         self.squares = self.squares
@@ -96,8 +115,8 @@ print(board.get_square_name(8))
 print(board.get_square_name(63))
 print(board.get_square_name(64))
 
-board.print_board()
-
-board.set_squares(1, "f4")
+board.set_square(1, "f4")
 
 board.print_board()
+print()
+board.print_board(True)
